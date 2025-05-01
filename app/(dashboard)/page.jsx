fd39d5e99014559c70d8a0794077ad12a9786134
jkClyde <aquin.randall@gmail.com>
2025-05-02 05:33:43 +0800

    try Fix

--- app/(dashboard)/page.jsx
+++ app/(dashboard)/page.jsx
@@ -1,25 +1,9 @@
-import Project from "@/models/Project";
-import connectDB from "@/config/database";
-import { ProjectDashboard } from "@/components/ProjectDashboard/ProjectDashboard";
+import Fetchdashboard from "./fetchprojects"
 
-const page = async () => {
-  await connectDB();
-  
-  // Fetch projects and convert to plain objects
-  const projects = await Project.find({}).lean();
-  
-  // Stringify and parse to ensure plain objects
-  const sanitizedProjects = JSON.parse(JSON.stringify(projects));
-
-  return <ProjectDashboard initialProjects={sanitizedProjects} />;
+const page = () => {
+  return (
+    <Fetchdashboard/>
+  )
 }
 
-export default page;
-
-/*
-  #305335F
-  #090F13
-  #153B47
-  #3D606E
-  #09191F
-*/ 
+export default page
